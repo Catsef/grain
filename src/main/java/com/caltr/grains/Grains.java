636bbd140a98b6eb017af569b81c7fa1a661f40a
Catsef <96400367+Catsef@users.noreply.github.com>
2024-03-02 14:13:58 +0800

    GRAINS

---
+++ src/main/java/com/caltr/grains/Grains.java
@@ -0,0 +1,17 @@
+package com.caltr.grains;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Grains extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
